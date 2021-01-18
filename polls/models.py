020a3b01c4d95f75270ca2431492a2b70185dcff
Sean Dougherty <sean@lupinehook.com>
2021-01-18 10:11:26 -0800

    moved polls templates into polls dir so it could be packaged and used elsewhere

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
         meow = timezone.now()
         return meow - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
 
+    was_published_recently.admin_order_field = "pub_date"
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published Recently?"
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
